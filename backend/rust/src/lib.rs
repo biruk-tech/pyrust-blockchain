8fdd0d79befd598d6dfc4b26e6bd1a2a4e4b0b0c
Biruk Kebede <biruktechstacks.gmail.com>
2024-11-22 08:49:55 +0000

    Testing remote connection

--- backend/rust/src/lib.rs
+++ backend/rust/src/lib.rs
@@ -15,19 +15,3 @@ fn rust_backend(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(hash_message, m)?)?;
     Ok(())
 }
-
-
-// pub fn add(left: u64, right: u64) -> u64 {
-//     left + right
-// }
-
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-
-//     #[test]
-//     fn it_works() {
-//         let result = add(2, 2);
-//         assert_eq!(result, 4);
-//     }
-// }
